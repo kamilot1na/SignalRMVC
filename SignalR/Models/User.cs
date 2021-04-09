69aab55e383e452a81862c50ccd49d9f7bb7af88
Kamil Nazmiev <kamilotina2001@gmail.com>
2021-04-09 13:12:14 +0300

    First Commit

---
+++ SignalR/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalR.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Pass { get; set; }
+    } 
+}
